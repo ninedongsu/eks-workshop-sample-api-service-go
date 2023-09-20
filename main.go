50a68447b5fe601055ced192d2707294b9c5fc99
ninedongsu <46261333+ninedongsu@users.noreply.github.com>
2023-09-20 21:43:25 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Test3"}
+		res := &response{Message: "Hello World Test4"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
